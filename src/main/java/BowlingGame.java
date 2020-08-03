ece9e6a68540dc20bdb846ee44bdd8d3b90cfd9f
chentaiyu0336 <2868510447@qq.com>
2020-08-03 19:54:28 +0800

    add have_strike_or_spare case

--- src/main/java/BowlingGame.java
+++ src/main/java/BowlingGame.java
@@ -1,10 +1,31 @@
 import java.util.List;
 
 public class BowlingGame {
-    public Integer cal_points(List<Integer> notOnceStrikeOrSpare) {
+    static final Integer FULL_POINT = 10;
+    public Integer cal_points(List<Integer> listShot) {
         Integer sumPoint=0;
-        for(Integer i:notOnceStrikeOrSpare) {
-            sumPoint+=i;
+//        for(Integer i:notOnceStrikeOrSpare) {
+//            sumPoint+=i;
+//        }
+        Integer roundTimes=0; // 轮数
+        for(int i=0;i<listShot.size();i++) {
+            Integer shotPointSingleCur=listShot.get(i);
+            Integer shotPointSingleNext=listShot.get(i+1);
+            if(shotPointSingleCur==FULL_POINT) { // 全中
+                sumPoint+=(FULL_POINT+shotPointSingleNext+listShot.get(i+2));
+            }
+            else if((shotPointSingleCur+shotPointSingleNext)==FULL_POINT) { // 补中
+                sumPoint+=(FULL_POINT+listShot.get(i+2));
+                i++;
+            }
+            else {
+                sumPoint+=(shotPointSingleCur+shotPointSingleNext);
+                i++;
+            }
+            roundTimes++;
+            if(roundTimes==10) {
+                break;
+            }
         }
         return sumPoint;
     }
--- src/test/java/BowlingGameTest.java
+++ src/test/java/BowlingGameTest.java
@@ -17,4 +17,15 @@ public class BowlingGameTest {
         //Then
         assertEquals("40",point.toString());
     }
+
+    @Test
+    void should_have_strike_or_spare() {
+        //Given
+        BowlingGame bowlingGame=new BowlingGame();
+        List<Integer> haveStrikeOrSpare= Arrays.asList(10,2,3,2,4,10,10,10,8,2,10,10,2,2);
+        //When
+        Integer point=bowlingGame.cal_points(haveStrikeOrSpare);
+        //Then
+        assertEquals("164",point.toString());
+    }
 }
